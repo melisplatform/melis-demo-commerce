9527f41ec0a9b9fa5ecac89528b78a39d2283013
DESKTOP-E78O66O\cmaka <crismakabenta@gmail.com>
2018-05-11 11:22:43 +0800

    Add default product on product show plugin

--- src/MelisDemoCommerce/Listener/SiteProductShowPluginListener.php
+++ src/MelisDemoCommerce/Listener/SiteProductShowPluginListener.php
@@ -23,6 +23,7 @@ class SiteProductShowPluginListener implements ListenerAggregateInterface
             '*',
             array(
                 'MelisCommerceProductShowPlugin_melistemplating_plugin_end',
+                'MelisCommerceProductShowPlugin_melistemplating_plugin_start',
             ),
             function($e){
                 // Getting the Service Locator from param target
@@ -30,101 +31,101 @@ class SiteProductShowPluginListener implements ListenerAggregateInterface
                 
                 // Getting the Datas from the Event Parameters
                 $params = $e->getParams();
-                
-                // Checking the DemoCommerce template assigned to the ProductShow plugin
-                if ($params['view']->getTemplate() == 'MelisDemoCommerce/plugin/show-product')
-                {
-                    $viewVariables = $params['view']->getVariables();
-                    
-                    $product = $viewVariables->product;
-                    
-                    $pluginConfig = $params['pluginFronConfig'];
-                    
-                    if (!empty($product->getId()))
-                    {
-                        $productId = $product->getId();
-                        
-                        $container = new Container('melisplugins');
-                        $langId = $container['melis-plugins-lang-id'];
-                        
-                        $variantSvc = $sm->get('MelisComVariantService');
-                        $currencySvc = $sm->get('MelisComCurrencyService');
-                        $currency = $currencySvc->getDefaultCurrency();
-                        
-                        $variant = array();
-                        $variantId = null;
-                        $action = null;
-                        $selection = array();
-                        
-                        // Getting the Site config "MelisDemoCommerce.config.php"
-                        $siteConfig = $sm->get('config');
-                        $siteConfig = $siteConfig['site']['MelisDemoCommerce'];
-                        $siteDatas = $siteConfig['datas'];
-                        
-                        $countryId = $siteDatas['site_country_id'];
-                        
-                        if ($productId)
-                        {
-                            $variant = $variantSvc->getMainVariantByProductId($productId, $langId, $countryId);
-                            
-                            if(is_null($variant))
-                            {
-                                
-                                $variant = $variantSvc->getVariantListByProductId($productId, $langId, $countryId);
-                                $variant = !empty($variant)? $variant[0] : $variant;
-                            }
-                            
-                            if(!empty($variant))
-                            {
-                                $tmp = $variant->getAttributeValues();
-                                
-                                usort($tmp, function($a, $b)
-                                {
-                                    return strcmp($a->atval_attribute_id, $b->atval_attribute_id);
-                                });
-                                
-                                foreach($tmp as $val)
-                                {
-                                    $action = $val->atval_attribute_id;
-                                    $selection[$val->atval_attribute_id] = $val->atval_id;
+
+                // Getting the Site config "MelisDemoCommerce.config.php"
+                $siteConfig = $sm->get('config');
+                $siteConfig = $siteConfig['site']['MelisDemoCommerce'];
+                $siteDatas = $siteConfig['datas'];
+
+                if(isset($params['view'])) {
+                    // Checking the DemoCommerce template assigned to the ProductShow plugin
+                    if ($params['view']->getTemplate() == 'MelisDemoCommerce/plugin/show-product') {
+                        $viewVariables = $params['view']->getVariables();
+
+                        $product = $viewVariables->product;
+
+                        $pluginConfig = $params['pluginFronConfig'];
+
+                        if (!empty($product->getId())) {
+                            $productId = $product->getId();
+
+                            $container = new Container('melisplugins');
+                            $langId = $container['melis-plugins-lang-id'];
+
+                            $variantSvc = $sm->get('MelisComVariantService');
+                            $currencySvc = $sm->get('MelisComCurrencyService');
+                            $currency = $currencySvc->getDefaultCurrency();
+
+                            $variant = array();
+                            $variantId = null;
+                            $action = null;
+                            $selection = array();
+
+                            $countryId = $siteDatas['site_country_id'];
+
+                            if ($productId) {
+                                $variant = $variantSvc->getMainVariantByProductId($productId, $langId, $countryId);
+
+                                if (is_null($variant)) {
+
+                                    $variant = $variantSvc->getVariantListByProductId($productId, $langId, $countryId);
+                                    $variant = !empty($variant) ? $variant[0] : $variant;
                                 }
-                                
-                                $variantId = $variant->getId();
+
+                                if (!empty($variant)) {
+                                    $tmp = $variant->getAttributeValues();
+
+                                    usort($tmp, function ($a, $b) {
+                                        return strcmp($a->atval_attribute_id, $b->atval_attribute_id);
+                                    });
+
+                                    foreach ($tmp as $val) {
+                                        $action = $val->atval_attribute_id;
+                                        $selection[$val->atval_attribute_id] = $val->atval_id;
+                                    }
+
+                                    $variantId = $variant->getId();
+                                }
                             }
+
+                            $viewRender = $sm->get('ViewRenderer');
+                            $pluginManger = $sm->get('ControllerPluginManager');
+
+                            // Adding MelisCommerceAttributesShowPlugin to ProductShowPlugin
+                            $attributeShowPlugin = $pluginManger->get('MelisCommerceAttributesShowPlugin');
+                            $pluginParams = array(
+                                'pageId' => $pluginConfig['pageId'],
+                                'id' => 'attributes_' . $pluginConfig['id'],
+                                'template_path' => 'MelisDemoCommerce/plugin/show-attributes',
+                                'm_product_id' => $product->getId(),
+                                'm_selected_attr' => $selection,
+                            );
+
+                            $attributePlugin = $attributeShowPlugin->render($pluginParams);
+                            $viewVariables->attribute = $viewRender->render($attributePlugin);
+
+                            // Adding MelisCommerceAddToCartPlugin to ProductShowPlugin
+                            $addToCartPlugin = $pluginManger->get('MelisCommerceAddToCartPlugin');
+                            $pluginParams = array(
+                                'pageId' => $pluginConfig['pageId'],
+                                'id' => 'addToCart_' . $pluginConfig['id'],
+                                'template_path' => 'MelisDemoCommerce/plugin/show-add-to-cart',
+                                'm_variant_id' => $variantId,
+                                'm_variant_country' => $countryId,
+                            );
+
+                            $addToCartPlugin = $addToCartPlugin->render($pluginParams);
+                            $viewVariables->addToCart = $viewRender->render($addToCartPlugin);
+
+
+                            $viewVariables->product_variant = $variant;
+                            $viewVariables->currency = $currency;
                         }
-                        
-                        $viewRender = $sm->get('ViewRenderer');
-                        $pluginManger = $sm->get('ControllerPluginManager');
-                        
-                        // Adding MelisCommerceAttributesShowPlugin to ProductShowPlugin
-                        $attributeShowPlugin =  $pluginManger->get('MelisCommerceAttributesShowPlugin');
-                        $pluginParams = array(
-                            'pageId' => $pluginConfig['pageId'],
-                            'id' => 'attributes_'.$pluginConfig['id'],
-                            'template_path' => 'MelisDemoCommerce/plugin/show-attributes',
-                            'm_product_id' => $product->getId(),
-                            'm_selected_attr' => $selection,
-                        );
-                        
-                        $attributePlugin = $attributeShowPlugin->render($pluginParams);
-                        $viewVariables->attribute = $viewRender->render($attributePlugin);
-                        
-                        // Adding MelisCommerceAddToCartPlugin to ProductShowPlugin
-                        $addToCartPlugin =  $pluginManger->get('MelisCommerceAddToCartPlugin');
-                        $pluginParams = array(
-                            'pageId' => $pluginConfig['pageId'],
-                            'id' => 'addToCart_'.$pluginConfig['id'],
-                            'template_path' => 'MelisDemoCommerce/plugin/show-add-to-cart',
-                            'm_variant_id' => $variantId,
-                            'm_variant_country' => $countryId,
-                        );
-                        
-                        $addToCartPlugin = $addToCartPlugin->render($pluginParams);
-                        $viewVariables->addToCart = $viewRender->render($addToCartPlugin);
-                        
-                       
-                        $viewVariables->product_variant = $variant;
-                        $viewVariables->currency = $currency;
+                    }
+                }else{
+                    if(!isset($params['m_product_id'])) {
+                        $params['m_product_id'] = $siteDatas['product_show_default_id'];
+                        $params['m_product_country'] = $siteDatas['site_country_id'];
                     }
                 }
             },
