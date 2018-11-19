085f954b6214c65ce52278e176a63cf97974eff1
bryan <rbbrioso28@gmail.com>
2018-11-19 18:12:13 +0800

    Removed unused codes

--- src/MelisDemoCommerce/Controller/ComLoginController.php
+++ src/MelisDemoCommerce/Controller/ComLoginController.php
@@ -70,9 +70,6 @@ class ComLoginController extends BaseController
         $this->layout()->setVariables(array(
             'pageJs' => array(
                 '/MelisDemoCommerce/js/melisSiteHelper.js',
-//                '/MelisDemoCommerce/js/login.js',
-
-//                '/MelisDemoCommerce/js/registration.js',
             ),
         ));
         
