225b17120e618683e8a6bdfda5dbe3033d404a04
fparan <fparan@melistechnology.com>
2019-06-20 19:19:47 +0800

    #0003955 - No view when there is no order

--- language/en_EN.php
+++ language/en_EN.php
@@ -1,5 +1,6 @@
 <?php 
     return array(
     	'tr_MelisDemoCommerce_name' => 'MelisDemoCommerce',
+        'tr_melisdemocommerce_general_no_result_found' => 'No result found',
     );
 ?>
--- language/fr_FR.php
+++ language/fr_FR.php
@@ -1,5 +1,6 @@
 <?php 
     return array(
     	'tr_MelisDemoCommerce_name' => 'MelisDemoCommerce',
+        'tr_melisdemocommerce_general_no_result_found' => 'Aucun résultat trouvé',
     );
 ?>
