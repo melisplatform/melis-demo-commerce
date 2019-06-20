5edfa9af2ebcb5931e3d79b2f24ee482cdd48f81
fparan <fparan@melistechnology.com>
2019-06-20 17:49:50 +0800

    removed fieldset on DemoSiteFieldCollection View Helper

--- src/MelisDemoCommerce/Form/View/Helper/DemoSiteFieldCollection.php
+++ src/MelisDemoCommerce/Form/View/Helper/DemoSiteFieldCollection.php
@@ -3,9 +3,20 @@
 namespace MelisDemoCommerce\Form\View\Helper;
  
 use Zend\Form\View\Helper\FormCollection;
+use Zend\Form\ElementInterface;
  
 class DemoSiteFieldCollection extends FormCollection
 {
     protected $defaultElementHelper = 'DemoSiteFieldRow';
- 
+
+    public function __invoke(ElementInterface $element = null, $wrap = false)
+    {
+        if (! $element) {
+            return $this;
+        }
+
+        $this->setShouldWrap($wrap);
+
+        return $this->render($element);
+    }
 }
