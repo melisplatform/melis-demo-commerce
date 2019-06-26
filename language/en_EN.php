5d45c4ce25eab1bc5f2f7288d777438c9ffd69c9
Cris <crismakabenta@gmail.com>
2019-06-26 11:24:11 +0800

    Added translation

--- language/en_EN.php
+++ language/en_EN.php
@@ -2,5 +2,7 @@
     return array(
     	'tr_MelisDemoCommerce_name' => 'MelisDemoCommerce',
         'tr_melisdemocommerce_general_no_result_found' => 'No result found',
+
+        'tr_MelisDemoCommerce_no_product_available' => 'No product available',
     );
 ?>
--- language/fr_FR.php
+++ language/fr_FR.php
@@ -2,5 +2,7 @@
     return array(
     	'tr_MelisDemoCommerce_name' => 'MelisDemoCommerce',
         'tr_melisdemocommerce_general_no_result_found' => 'Aucun résultat trouvé',
+
+        'tr_MelisDemoCommerce_no_product_available' => 'No product available',
     );
 ?>
