b1375e6a6ff02f78f92770323673eb976c0e28a3
jzabate <jzabate@melistechnology.com>
2024-07-04 12:14:32 +0800

    checking issue 6608

--- public/js/login.js
+++ public/js/login.js
@@ -1,35 +1,42 @@
-$(function(){
-	$("#submit-login-form-btn").on("click", function(){
+$(function() {
+	$("#submit-login-form-btn").on("click", function() {
 		var btn = $(this);
 		
-		// Hidding all alerts
-		$(".login-form .alert").addClass("hidden");
-		
-		// convert the serialized form values into an array
-		var datastring = $("form#login").serializeArray();
-		
-		// serialize the new array and send it to server
-		datastring = $.param(datastring);;
-		$.ajax({
-			type        : 'POST', 
-	        url         : '/MelisDemoCommerce/ComLogin/submitLogin',
-	        data        : datastring,
-	        dataType    : 'json',
-	        encode		: true
-		}).done(function(data){
-			if(data.success){
-				// Showing the Success result for submitting form
-				$(".contact-info .alert-success").removeClass("hidden");
-				// Reseting/make empty the Contact form
-				$('form#login')[0].reset();
-				// Redirect after login authentication success
-				window.location = data.redirect_link;
-			}else{
-				// Showing the Error result for submitting form
-				$(".login-form .alert-danger").removeClass("hidden");
-				// Highlighting the input fields that has an error using the custom helper
-                forms.checkForm(".login-form", data.errors);
-			}
-		});
+			// Hidding all alerts
+			$(".login-form .alert").addClass("hidden");
+			
+			// convert the serialized form values into an array
+			var datastring = $("form#login").serializeArray();
+			
+			// serialize the new array and send it to server
+			datastring = $.param(datastring);
+
+			$.ajax({
+				type        : 'POST', 
+				url         : '/MelisDemoCommerce/ComLogin/submitLogin',
+				data        : datastring,
+				dataType    : 'json',
+				encode		: true
+			}).done(function(data) {
+				console.log(`data.success: `, data.success);
+				if ( data.success ) {
+					// Showing the Success result for submitting form
+					$(".contact-info .alert-success").removeClass("hidden");
+					// Reseting/make empty the Contact form
+					$('form#login')[0].reset();
+					// Redirect after login authentication success
+					window.location = data.redirect_link;
+				}
+				else {
+					// Showing the Error result for submitting form
+					$(".login-form .alert-danger").removeClass("hidden");
+					// Highlighting the input fields that has an error using the custom helper
+					forms.checkForm(".login-form", data.errors);
+				}
+			}).fail(function(xhr, textStatus, errorThrown) {
+				console.log({xhr});
+				console.log({textStatus});
+				console.log({errorThrown});
+			});
 	});
 });
